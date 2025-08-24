94490e04fd3ff512f037581f5725cf0fe7ade17e
NEERAJ RAJ <34566622+devilraj98@users.noreply.github.com>
2025-08-24 19:19:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
 
-
-resource "aws_instance" "refobj" {
-  ami = "ami-0861f4e788f5069dd"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name  = "Lab-admin"
-  }
-  
-  }
 #------------------------------
 # Create a VPC
 #-----------------------------
